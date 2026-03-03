ca0edf83131bd0f5558aaebbed616916990dec8a
julianaPineda <juliana.pineda@col.vueling.com>
2026-03-03 16:23:27 -0500

    practica variables

--- js/variables.js
+++ js/variables.js
@@ -1,2 +1,27 @@
 console.log("Variables in JavaScript");
 
+// ¿ Que es una variable? Es una cajita donde se guardan valores 
+// string, number, booleanos, 
+// let - const
+// let cuando el valor de la variable puede cambiar
+// const cuando el valor nunca cambia en el tiempo 
+
+let age = 10;
+console.log("age", age);
+
+const firstName = "Juliana"
+console.log("mi nombre es :", firstName);
+
+// firstName = "ana";
+
+//Declarar variables que describan a una persona e imprimir en consola
+
+const nombre = "Juliana";
+const apellido = "Pineda";
+let edad = 25;
+let peso = 65;
+const estatura = 1.55;
+let isMarried = true;
+const esFemenino = true;
+let esSoltero = false; 
+console.log("Este es mi nombre:", nombre, "mi edad es:", edad, "mi peso es:", peso);
