5e7c63305ed1abfc726e07399d98f9bfedd6dd6d
julianaPineda <juliana.pineda@col.vueling.com>
2026-03-05 16:24:55 -0500

    condicionales

--- js/variables.js
+++ js/variables.js
@@ -18,7 +18,7 @@ console.log("mi nombre es :", firstName);
 
 const nombre = "Juliana";
 const apellido = "Pineda";
-let edad = 25;
+// let edad = 25;
 let peso = 65;
 const estatura = 1.55;
 let isMarried = true;
@@ -26,3 +26,22 @@ const esFemenino = true;
 let esSoltero = false; 
 let nombreHija = "Antonella";
 console.log("Mi nombre es:", nombre, " mi edad es:", edad, " mi peso es:", peso , "mi estatura es:", estatura, "estoy casada?", isMarried + " " + "Soy mujer:", esFemenino + " Soy soltera:", esSoltero, "nombre hija:", nombreHija);
+
+//  si tengo dni y soy mayor de edad  y tengo nacionalidad
+// imrimir en consola puedo votar
+//  si  tengo nacionalidad  y sy mayor de edad y no tengo dni
+// imprimir debes ir a la registraduria
+// en caso contrario imprimir en consola no puedes votar
+
+const tengoDni = true;
+const edad = 18;
+const nacionalidad = true;
+
+if (tengoDni && edad >= 18 && nacionalidad){
+    console.log("puedo votar");
+} if (nacionalidad && edad >=18 && !tengoDni){
+    console.log("debes ir a la registraduria")
+}
+else (
+    console.log("no puedes votar")
+)
