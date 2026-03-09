de95cdaef6bc08b67f7141ed788fbda1af99c27b
julianaPineda <juliana.pineda@col.vueling.com>
2026-03-09 16:15:44 -0500

    operador ternario

--- js/arrowFunctions.js
+++ js/arrowFunctions.js
@@ -1,4 +1,4 @@
-// Es lo mismo que una función tradicional pero con una sintaxis más corta,
+// ARROW FUNCTION: Es lo mismo que una función tradicional pero con una sintaxis más corta,
 
 console.log("Bienvenidos arrow functions")
 
@@ -22,3 +22,19 @@ const verifyIsMajor = (age) => {
 
 console.log("console.log desde funcion", verifyIsMayor(19));
 console.log("console.log desde arrow funcion", verifyIsMajor(19));
+
+// OPERADOR TERNARIO: Es el mismo if else con otra sintaxis
+// Se usa cuando son validaciones pequeñas para las validaciones grandes se usan el if y el else
+
+// expresion ? console.log("true"): console.log("aqui");
+
+const verifyedad = (age) => {
+    return age >= 18 ? "es mayor de edad" : "es menor de edad"
+}
+
+console.log(verifyedad(19));
+
+
+
+
+
